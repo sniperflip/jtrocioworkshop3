ac811031f09fd7358b4b42c2c4c3961f0f161719
sniperflip <johanntrocio29@gmail.com>
2017-12-16 13:37:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Johann Trocio - Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Rock Paper Scissors</h1>
+
+<p>
+<a href="login.php">Please Click to Log In</a>
+</p>
+
+
+</div>
+</body>
+
